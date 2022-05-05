bae621cb8a74579bba1f8ffbba651a384239ec37
Guillermo Amutio Duarte <guillermo.amutio-duarte@capgemini.com>
2022-05-05 16:39:51 +0200

    Window conf

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,14 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+use tauri::Manager;
 
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+      app.windows().iter().for_each(|(_, window)| {
+        window_shadows::set_shadow(&window, true).unwrap_or(());
+      });
+
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
